e5c234b7f7d835cb66cd36a5ab160330eef9bf69
cblythe <ucfblythe83@gmail.com>
2018-03-06 17:08:11 -0500

    Modifying output graph.

--- src/class_net.py
+++ src/class_net.py
@@ -23,11 +23,12 @@ class ClassNet(nn.Module):
         super(ClassNet, self).__init__()
 
         self.class_size = class_size
-        self.hidden1 = nn.Linear(32, 256, bias=False)
-        self.norm1 = nn.BatchNorm1d(256)
-        self.out = nn.Linear(256, class_size, bias=False)
+        self.layer1 = nn.Linear(32, 256)
+        self.layer2 = nn.Linear(256, 256)
+        self.layer3 = nn.Linear(256, class_size)
+        self.dropout = nn.Dropout(p=0.5)
         self.relu = nn.ReLU()
-        self.logits = nn.Softmax()
+        self.softmax = nn.Softmax(dim=1)
 
         self.tcn = tcn
 
@@ -40,6 +41,7 @@ class ClassNet(nn.Module):
             self.learning = float(cnet_settings["learning"])
             self.momentum = float(cnet_settings["momentum"])
             self.batch_size = int(cnet_settings["batch"])
+            self.frames = int(cnet_settings["num_frames"])
             self.epochs = int(cnet_settings["epochs"])
             self.train_cnet()
         else:
@@ -47,15 +49,17 @@ class ClassNet(nn.Module):
             self.load_cnet()
 
 
-    def forward(self, x):
-        layer = self.hidden1(x)
+    def forward(self, layer):
+        layer = self.layer1(layer)
         layer = self.relu(layer)
-        layer = self.norm1(layer)
+        layer = self.dropout(layer, 0.5)
+        layer = self.layer2(layer)
+        layer = self.relu(layer)
+        layer = self.dropout(layer, 0.5)
+        layer = self.layer3(layer)
+        predictions = self.softmax(layer)
 
-        layer = self.logits(layer)
-        prob = self.softmax(layer)
-
-        return prob
+        return predictions
 
     def save_model(self, temp_epoch=None):
         if not os.path.exists("./saves"):
@@ -68,7 +72,7 @@ class ClassNet(nn.Module):
         learning_string = learning_string[learning_string.find('.'):]
 
         filename = "cnet_epochs_{}_batch_{}_frames_{}_learning_{}.pt".format(temp_epoch, self.batch_size,
-                                                                             self.num_frames, learning_string)
+                                                                             self.frames, learning_string)
 
         save(self, "./saves/"+filename)
 
@@ -99,26 +103,27 @@ class ClassNet(nn.Module):
 
         return batch_results
 
-    def validate(self, dataset):
+    def validate_cnet(self, dataset):
 
         data_loader = DataLoader(
             dataset=dataset,
             batch_size=self.batch_size,
-            shuffle=False,
+            shuffle=True,
             pin_memory=self.is_cuda
         )
+
+        self.eval()
 
         total = 0
         num_correct = 0
         for batch, labels in data_loader:
 
             output = self.process_batch(batch)
-            predictions = torch.max(output.data)
-
-            num_correct += (predictions == labels).sum()
+            _, predicted = torch.max(output.data, 1)
             total += labels.size(0)
+            num_correct += (predicted.cpu() == labels).sum()
 
-            del predictions
+            del output
             torch.cuda.empty_cache()
             gc.collect()
 
@@ -129,87 +134,7 @@ class ClassNet(nn.Module):
         transform = transforms.Compose([transforms.CenterCrop(224), transforms.Resize(112), transforms.ToTensor()])
         path = os.path.abspath("./")
 
-        training = IXMASDataset(path, self.training_collections, transform=transform, verbose=False)
-        training.set_triplet_flag(False)
-
-        validation = IXMASDataset(path, self.validation_collection, transform=transform, verbose=False)
-        validation.set_triplet_flag(False)
-
-        print("-----Starting CNET Training-----")
-        data_loader = DataLoader(
-            dataset=training,
-            batch_size=self.batch_size,
-            shuffle=True,
-            pin_memory=self.is_cuda
-        )
-
-        if self.is_cuda:
-            self.cuda()
-
-        optimizer = optim.SGD(self.parameters(), lr=self.learning, momentum=self.momentum)
-        learning_rate_scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[15, 25, 35], gamma=0.1)
-        classifier = nn.CrossEntropyLoss()
-
-        historical = []
-        accuracy = []
-        total_time = 0
-
-        for i in range(self.epochs):
-            print("Epoch {}:".format(i))
-            start_time = time.time()
-            losses = []
-            learning_rate_scheduler.step()
-
-            for (batch, labels) in data_loader:
-
-                output = self.process_batch(batch)
-
-                optimizer.zero_grad()
-                loss = classifier(output, labels)
-                loss_data = loss.data.cpu().numpy()[0]
-                losses.append(loss_data)
-                loss.backward()
-
-                del output
-                torch.cuda.empty_cache()
-                gc.collect()
-
-            mean_loss = np.mean(losses)
-            print("\tDistance Loss: " + str(mean_loss))
-            historical.append(mean_loss)
-
-            val_loss = self.validate_tcn(validation)
-            print("\tValidation Loss: " + str(val_loss))
-            accuracy.append(val_loss)
-
-            end_time = time.time() - start_time
-            total_time += end_time
-            print("\tEpoch Time:{}".format(timedelta(seconds=end_time)))
-
-            next_epoch = i + 1
-            if next_epoch % 5 == 0 and next_epoch != 0:
-                self.save_model()
-
-                x = [j for j in range(0, next_epoch)]
-                plt.title("Iterations vs Loss")
-                plt.xlabel("Iterations")
-                plt.ylabel("Distance Loss")
-                plt.plot(x, historical, marker='o')
-                plt.show()
-
-                plt.title("Iterations vs Validation Loss")
-                plt.xlabel("Iterations")
-                plt.ylabel("Validation Loss")
-                plt.plot(x, accuracy, marker='o')
-                plt.show()
-
-        print("Total Time: {}".format(timedelta(seconds=total_time)))
-        print("----Completed CNET Training-----")
-
-
-    def test_cnet(self):
-        transform = transforms.Compose([transforms.CenterCrop(224), transforms.Resize(112), transforms.ToTensor()])
-        path = os.path.abspath("./")
+        self.train()
 
         training = IXMASDataset(path, self.training_collections, transform=transform, verbose=False)
         training.set_triplet_flag(False)
@@ -228,8 +153,7 @@ class ClassNet(nn.Module):
         if self.is_cuda:
             self.cuda()
 
-        optimizer = optim.SGD(self.parameters(), lr=self.learning, momentum=self.momentum)
-        learning_rate_scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[15, 25, 35], gamma=0.1)
+        optimizer = optim.Adam(self.parameters(), lr=self.learning)
         classifier = nn.CrossEntropyLoss()
 
         historical = []
@@ -240,17 +164,19 @@ class ClassNet(nn.Module):
             print("Epoch {}:".format(i))
             start_time = time.time()
             losses = []
-            learning_rate_scheduler.step()
 
             for (batch, labels) in data_loader:
 
                 output = self.process_batch(batch)
 
+                labels_tensor = Variable(labels).cuda()
+
                 optimizer.zero_grad()
-                loss = classifier(output, labels)
+                loss = classifier(output, labels_tensor)
                 loss_data = loss.data.cpu().numpy()[0]
                 losses.append(loss_data)
                 loss.backward()
+                optimizer.step()
 
                 del output
                 torch.cuda.empty_cache()
@@ -260,7 +186,7 @@ class ClassNet(nn.Module):
             print("\tDistance Loss: " + str(mean_loss))
             historical.append(mean_loss)
 
-            val_loss = self.validate_tcn(validation)
+            val_loss = self.validate_cnet(validation)
             print("\tValidation Loss: " + str(val_loss))
             accuracy.append(val_loss)
 
